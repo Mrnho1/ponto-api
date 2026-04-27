84d359b572f0de29006dce4fd71d73fc2359f54b
Lucas Marinho <ls.marinho36@gmail.com>
2026-04-27 13:55:14 -0300

    add comment in main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from app.core.database import Base, engine
 from app.controllers import ponto_controller
-
+#Manda criar as tabelas no banco com base nos models
 Base.metadata.create_all(bind=engine)
 
+#Aqui iremos criar a aplicação
 app = FastAPI(title="API de Ponto Eletrônico")
-
+#Conecta os endpoints na aplicação
 app.include_router(ponto_controller.router)
