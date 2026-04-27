0e65a4b2db163ece39b0d2434b1381289c85acce
Lucas Marinho <ls.marinho36@gmail.com>
2026-04-27 15:17:54 -0300

    add comment in model.py

--- app/models/ponto_model.py
+++ app/models/ponto_model.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, DateTime
 from datetime import datetime
 from app.core.database import Base
-
+#Representa uma tabela no banco
 class Ponto(Base):
-    __tablename__ = "pontos"
+    __tablename__ = "pontos" #Nome tabela
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True) 
     data_hora = Column(DateTime, default=datetime.utcnow)
