84f9b137febe0d55c3f44b36401884046cf14f58
Lucas Marinho <ls.marinho36@gmail.com>
2026-04-27 13:58:46 -0300

    add comment in database.py

--- app/core/database.py
+++ app/core/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./ponto.db"
-
+#Conecta com o SQLite
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
+#Criar sessões, conexões temporárias com o banco
 SessionLocal = sessionmaker(bind=engine)
-
+#Base que todos os models herdam
 Base = declarative_base()
