8c57a7ddd899953abf5094f6170f50a0999663d8
Lucas Marinho <ls.marinho36@gmail.com>
2026-04-27 15:22:14 -0300

    add comment in schema.py

--- app/schemas/ponto_schema.py
+++ app/schemas/ponto_schema.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 from datetime import datetime
-
+#Modo de resposta da API
 class PontoResponse(BaseModel):
     id: int
     data_hora: datetime
-
+    #Igual ao mapStruct
     class Config:
-        orm_mode = True
+        orm_mode = True #Permite converter automaticamente do model
