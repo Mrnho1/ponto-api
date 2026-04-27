2e9e2495003ea66d8c58c58d1f025d0b1384ef9c
Lucas Marinho <ls.marinho36@gmail.com>
2026-04-27 15:31:30 -0300

    add comment

--- app/repositories/ponto_repository.py
+++ app/repositories/ponto_repository.py
@@ -3,9 +3,9 @@ from app.models.ponto_model import Ponto
 
 def criar_ponto(db: Session):
     ponto = Ponto()
-    db.add(ponto)
-    db.commit()
-    db.refresh(ponto)
+    db.add(ponto) #Adiciona no banco
+    db.commit() #Salva no banco
+    db.refresh(ponto) #Atualiza os objetos no Banco
     return ponto
 
 def listar_pontos(db: Session):
